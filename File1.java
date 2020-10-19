a57774e584a3fd2f2a24494d75a9a3bb7007333c
Mahesh Verma <mahesh.verma@sungardas.com>
2020-10-19 10:51:46 +0530

    Final Commit for today

--- File1.java
+++ File1.java
@@ -3,3 +3,5 @@ We are learning new commands for GIT Hub
 Will Try and share it will all.
 I am Superman and I am trying to to a Fast Forward Merge now in Git
 Lets see how this works
+This is a new line post the first Conflict merge.
+Lets check how this works
